0eeab8830fbab74c9127cf7a40df34c7720c112c
saif1431 <buttsaif539@gmail.com>
2025-09-20 00:46:52 +0500

    fix some issues

--- src/Components/HomeComponents/OurProcess.jsx
+++ src/Components/HomeComponents/OurProcess.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function OurProcess() {
   return (
-    <div className=' w-full h-fit bg-secondary px-4 lg:py-18 md-12 '>
-      <img className='max-w-7xl mx-auto w-full h-screen' src="/process.png" alt="" />
+    <div className=' w-full  h-fit bg-secondary px-4 lg:py-18 md-12 '>
+      <img className='max-w-8xl mx-auto w-full px-4 h-screen' src="/process.png" alt="" />
     </div>
   )
 }
