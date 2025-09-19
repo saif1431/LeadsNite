34d53df2405df787e5c6ab66a889e087b3bf6451
saif1431 <buttsaif539@gmail.com>
2025-09-20 01:00:56 +0500

    change image

--- src/Components/HomeComponents/OurProcess.jsx
+++ src/Components/HomeComponents/OurProcess.jsx
@@ -4,7 +4,7 @@ function OurProcess() {
   return (
     <div className=' w-full  h-fit bg-secondary px-4 lg:py-18  '>
       <div className='max-w-7xl mx-auto '>
-            <img className=' w-full h-screen' src="/process.png" alt="" />
+            <img className=' w-full h-screen' src="/process2.png" alt="" />
       </div>
     </div>
   )
