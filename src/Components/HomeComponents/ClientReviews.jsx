87c875cdbe52c6a7ef8a34a6235f464059fe4e7d
saif1431 <buttsaif539@gmail.com>
2025-10-20 15:46:43 +0500

    change the Height

--- src/Components/HomeComponents/ClientReviews.jsx
+++ src/Components/HomeComponents/ClientReviews.jsx
@@ -161,7 +161,7 @@ const ClientReviews = () => {
         {/* Desktop: Three Columns */}
         <div className="hidden md:grid overflow-hidden grid-cols-3 gap-6  ">
           {/* Column 1: Scroll Up */}
-          <div className="overflow-hidden mt-14 h-[600px]">
+          <div className="overflow-hidden mt-10 h-[600px]">
             <div className="scroll-up">
               {extendedColumn1.map((logo, index) => (
                 <div
@@ -207,7 +207,7 @@ const ClientReviews = () => {
           </div>
 
           {/* Column 3: Scroll Up */}
-          <div className="overflow-hidden mt-14 h-[600px]">
+          <div className="overflow-hidden mt-10 h-[600px]">
             <div className="scroll-up">
               {extendedColumn3.map((logo, index) => (
                 <div
