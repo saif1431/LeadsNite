d15d8d620c46aa87022575bd612188da297b4971
Saif Butt <buttsaif539@gmail.com>
2025-10-31 13:09:13 -0700

    Change some  Meta Tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from 'react-helmet'
 import HomePage from './pages/HomePage'
 import ServicePage from './pages/ServicePage'
 import AboutUsPage from './pages/AboutUsPage'
